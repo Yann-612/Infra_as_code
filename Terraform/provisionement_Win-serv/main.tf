e8bd4ed1bcaacd2e628f114f12c087e084b9ff3e
yannick <yannick.ahyoune@gmail.com>
2025-04-06 17:55:54 +0200

    Dossier du dossier srv_windows

--- Terraform/provisionement_Win-serv/main.tf
+++ Terraform/provisionement_Win-serv/main.tf
@@ -1,12 +1,34 @@
-
-## main.tf
-
+# This Terraform script provisions a Windows Server virtual machine in Azure with a public IP address and a network security group allowing RDP access.
+# The script includes the creation of a resource group, virtual network, subnet, network interface, and the virtual machine itself.
+# It also outputs the public IP address of the virtual machine.
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
 
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "Allow-RDP"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
@@ -29,7 +51,9 @@ resource "azurerm_network_interface" "example" {
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.2.10"
+    public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
 
@@ -62,9 +86,10 @@ resource "azurerm_public_ip" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
-  allocation_method = "Dynamic"
+  allocation_method = "Static"
+  sku               = "Standard"
 }
 
 output "vm_public_ip" {
-  value = azurerm_public_ip.public.ip_address
+  value = azurerm_public_ip.example.ip_address
 }
