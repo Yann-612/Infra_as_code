4b1b90f754026549c4cc2bfaf8cfc6e390eb4419
yannick <yannick.ahyoune@gmail.com>
2025-04-14 22:47:09 +0200

    ajout des workflows GitHub Actions

---
+++ github/provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+subscription_id = "ea74f97a-80cb-4d62-9ff9-04c1327d3880"
+client_id = "5fb41f78-d49e-4369-9ee8-e166f856c461" # AppId
+client_secret = "vvI8Q~xvP6_ynhR5U9MdFHNys8hzY.X3BPN.RbkA" # password
+tenant_id = "b7b023b8-7c32-4c02-92a6-c8cdaa1d189c" # tenant
+features {}
+}
