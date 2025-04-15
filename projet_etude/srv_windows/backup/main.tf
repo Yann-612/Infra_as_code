655abcfa60281b5b3fde9515871bc6cc6134b3a7
yannick <yannick.ahyoune@gmail.com>
2025-04-15 23:20:01 +0200

    test d'un nouveau code de déploiement serv Win 2022

--- projet_etude/srv_windows/backup/main.tf
+++ projet_etude/srv_windows/backup/main.tf
@@ -1,31 +1,29 @@
-# This Terraform script provisions a Windows Server virtual machine in Azure with a public IP address and a network security group allowing RDP access.
-# The script includes the creation of a resource group, virtual network, subnet, network interface, and the virtual machine itself.
-# It also outputs the public IP address of the virtual machine.
-
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+resource "azurerm_resource_group" "rg" {
+  name     = "my-windows-server-rg"
+  location = "East US 2" # Choisissez la région souhaitée
 }
 
-resource "azurerm_network_security_group" "example" {
-  name                = "example-nsg"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_virtual_network" "vnet" {
+  name                = "my-vnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "windows-server-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "Allow-RDP"
-    priority                   = 1000
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3389"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "Allow-winrm"
+    name                       = "AllowWinRM"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
@@ -36,51 +34,51 @@ resource "azurerm_network_security_group" "example" {
     destination_address_prefix = "*"
   }
 
-}
-
-
-resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.example.id
-  network_security_group_id = azurerm_network_security_group.example.id
-}
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = "10.0.2.10"
-    public_ip_address_id          = azurerm_public_ip.example.id
+  security_rule {
+    name                       = "AllowRDP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 }
 
-resource "azurerm_windows_virtual_machine" "example" {
-  name                = "example-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  size                = "Standard_F4"
-  admin_username      = "adminuser"
-  admin_password      = "Pa$$word1234!"
+resource "azurerm_public_ip" "pip" {
+  name                = "windows-server-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Dynamic" # Vous pouvez choisir "Static" si vous avez besoin d'une IP publique fixe
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "windows-server-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "primary"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = "windows-server-2022"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_DS1_v2" # Choisissez la taille de VM souhaitée
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.nic.id,
   ]
+  admin_username        = "adminuser" # Remplacez par le nom d'utilisateur souhaité
+  admin_password        = "P@$$wOrd1234!" # Remplacez par un mot de passe sécurisé
+  provisioner "local-exec" { # Ceci est déconseillé pour la production, utilisez plutôt des extensions ou des outils de configuration
+    command = "echo 'Warning: Using local-exec with plain text password. Consider using Azure Key Vault or other secure methods for password management.' > warning.txt"
+  }
 
   os_disk {
     caching              = "ReadWrite"
@@ -90,43 +88,18 @@ resource "azurerm_windows_virtual_machine" "example" {
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2022-Datacenter"
+    sku       = "2022-datacenter-azure-edition"
     version   = "latest"
   }
 }
 
-
-resource "azurerm_virtual_machine_extension" "winrm_config" {
-  name                 = "winrm-config"
-  virtual_machine_id   = azurerm_windows_virtual_machine.example.id
-  publisher            = "Microsoft.Compute"
-  type                 = "CustomScriptExtension"
-  type_handler_version = "1.10"
-
-  settings = <<SETTINGS
-    {
-      "commandToExecute": "powershell.exe -ExecutionPolicy Bypass -File C:\\Windows\\Temp\\configure_winrm.ps1"
-    }
-  SETTINGS
-
-  protected_settings = <<PROTECTED_SETTINGS
-    {
-      "script": "${base64encode(file("configure_winrm.ps1"))}"
-    }
-  PROTECTED_SETTINGS
+resource "azurerm_network_interface_security_group_association" "nic_nsg_association" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-
-
-resource "azurerm_public_ip" "example" {
-  name                = "example-public-ip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  allocation_method = "Static"
-  sku               = "Standard"
-}
-
-output "vm_public_ip" {
-  value = azurerm_public_ip.example.ip_address
+output "public_ip_address_id" {
+  value = azurerm_public_ip.pip.ip_address
+  description = "The public IP address of the Windows Server VM"
+  
 }
