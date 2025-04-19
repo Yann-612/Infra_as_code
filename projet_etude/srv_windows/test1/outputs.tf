698205e8dd1370d35dbb9a46309c879cc1a378ae
yannick <yannick.ahyoune@gmail.com>
2025-04-19 19:20:35 +0200

    nouveau code winrm_setup

--- projet_etude/srv_windows/test1/outputs.tf
+++ projet_etude/srv_windows/test1/outputs.tf
@@ -1,3 +1,8 @@
 output "public_ip_address" {
   value = azurerm_public_ip.public_ip.ip_address
 }
+
+output "vm_id" {
+  value       = azurerm_windows_virtual_machine.vm.id
+  description = "The ID of the virtual machine"
+}
