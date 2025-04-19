d53c3b55e0dad14b70b7c5b33ddff9b954a47efc
yannick <yannick.ahyoune@gmail.com>
2025-04-19 19:37:14 +0200

    nouveau code winrm_setup

--- projet_etude/srv_windows/test1/winrm_setup.ps1
+++ projet_etude/srv_windows/test1/winrm_setup.ps1
@@ -1,11 +1,22 @@
-# Configure WinRM for Ansible (insecure HTTP)
+# Activer WinRM en HTTP (non-HTTPS, attention à la sécurité)
+winrm quickconfig -force
+winrm set winrm/config/service/Auth '@{Basic="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 Set-Item -Path WSMan:\localhost\Service\AllowUnencrypted -Value $true
 Set-Item -Path WSMan:\localhost\Service\Auth\Basic -Value $true
-
-# Start the WinRM service
-Set-Service -Name WinRM -StartupType Automatic
-Start-Service -Name WinRM
-
-# Add firewall rule for WinRM
-New-NetFirewallRule -DisplayName "WinRM Inbound" -Direction Inbound -Protocol TCP -LocalPort 5985 -Action Allow
-
+Enable-PSRemoting -Force
+# Autoriser les connexions à distance
+Set-Item WSMan:\localhost\Service\AllowUnencrypted -Value $true
+Set-Item WSMan:\localhost\Service\Auth\Basic -Value $true
+# Autoriser les connexions à distance sur le pare-feu Windows
+New-NetFirewallRule -Name "WinRM HTTP" -DisplayName "WinRM HTTP" -Enabled True -Profile Any -Action Allow -Protocol TCP -LocalPort 5985
+# Autoriser les connexions à distance sur le pare-feu Windows (HTTPS)
+New-NetFirewallRule -Name "WinRM HTTPS" -DisplayName "WinRM HTTPS" -Enabled True -Profile Any -Action Allow -Protocol TCP -LocalPort 5986
+# Autoriser les connexions à distance sur le pare-feu Windows (WSMan)
+New-NetFirewallRule -Name "WSMan" -DisplayName "WSMan" -Enabled True -Profile Any -Action Allow -Protocol TCP -LocalPort 80, 443, 5985, 5986
+# Autoriser les connexions à distance sur le pare-feu Windows (HTTP)
+New-NetFirewallRule -Name "HTTP" -DisplayName "HTTP" -Enabled True -Profile Any -Action Allow -Protocol TCP -LocalPort 80
+# Autoriser les connexions à distance sur le pare-feu Windows (HTTPS)
+New-NetFirewallRule -Name "HTTPS" -DisplayName "HTTPS" -Enabled True -Profile Any -Action Allow -Protocol TCP -LocalPort 443
+# Autoriser les connexions à distance sur le pare-feu Windows (RDP)
+New-NetFirewallRule -Name "RDP" -DisplayName "RDP" -Enabled True -Profile Any -Action Allow -Protocol TCP -LocalPort 3389
