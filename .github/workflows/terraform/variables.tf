29e956c81cfdd77b4c126ad847a2486fa51927b0
Yann <yannick.ahyoune@gmail.com>
2025-05-17 17:16:14 +0200

    Update variables.tf

--- .github/workflows/terraform/variables.tf
+++ .github/workflows/terraform/variables.tf
@@ -28,13 +28,24 @@ variable "prefix" {
   type        = string
 }
 
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
 variable "client_secret" {
-  description = "Secret du client Azure"
+  description = "Azure Client Secret"
   type        = string
   sensitive   = true
 }
 
-variable "subscription_id" { type = string }
-variable "client_id" { type = string }
-variable "tenant_id" { type = string }
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
 
