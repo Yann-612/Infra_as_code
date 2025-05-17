496267afdc4363b6214c33fb78ce01f1506823bf
Yann <yannick.ahyoune@gmail.com>
2025-05-17 18:29:03 +0200

    Create variables.tf

---
+++ .github/workflows/variables.tf
@@ -0,0 +1,50 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Nom du Resource Group"
+}
+
+variable "location" {
+  type        = string
+  description = "Région Azure"
+}
+
+variable "vm_name" {
+  type        = string
+  description = "Nom de la machine virtuelle"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Nom d'utilisateur administrateur"
+}
+
+variable "ssh_public_key" {
+  type        = string
+  description = "Chemin vers la clé SSH publique"
+}
+
+variable "prefix" {
+  description = "Préfixe utilisé pour nommer les ressources"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
